2fe2ac77327bb8a61b20a8868501729ec99f4faa
charan2000 <44344358+charan2000@users.noreply.github.com>
2021-01-26 23:18:39 +0530

    leet code files

---
+++ LeetCode/reverseInteger.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class reverseInteger{
+    public static void main(String[], args){
+        Scanner sc = new Scanner(System.in);
+        int ovalue = sc.nextInt();
+        System.out.println(ovalue);
+    }
+}
